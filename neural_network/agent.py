4a6951615458d3426ad2e26c1a12daa76b7a84ea
lockr <doly2004e@yandex.ru>
2024-06-09 06:46:48 +0300

    some new files

---
+++ neural_network/agent.py
@@ -0,0 +1,82 @@
+import gym
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from collections import deque
+import zmq
+
+class RLAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0   # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = keras.Sequential()
+        model.add(keras.layers.Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(keras.layers.Dense(24, activation='relu'))
+        model.add(keras.layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        minibatch = np.random.choice(len(self.memory), batch_size)
+        for index in minibatch:
+            state, action, reward, next_state, done = self.memory[index]
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+if __name__ == "__main__":
+    state_size = 4  # пример
+    action_size = 2  # два сервера
+    agent = RLAgent(state_size, action_size)
+    
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    while True:
+        # Получение состояния от Nginx
+        message = socket.recv_json()
+        state = np.array(message['state']).reshape([1, state_size])
+        
+        # Выбор действия агентом
+        action = agent.act(state)
+        
+        # Отправка выбранного сервера обратно в Nginx
+        socket.send_string(str(action))
+        
+        # Обучение агента на опыте
+        next_state = np.array(message['next_state']).reshape([1, state_size])
+        reward = message['reward']
+        done = message['done']
+        agent.remember(state, action, reward, next_state, done)
+        if len(agent.memory) > 32:
+            agent.replay(32)
---
+++ ngx_TT_module/ngx_http_rl_balancer_module.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright (C) 2024 Example Author
+ */
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <zmq.h>  // ZeroMQ для IPC
+
+static ngx_int_t ngx_http_rl_balancer_init(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t *us);
+static ngx_int_t ngx_http_rl_balancer_init_peer(ngx_http_request_t *r, ngx_http_upstream_srv_conf_t *us);
+static ngx_int_t ngx_http_rl_balancer_get_peer(ngx_peer_connection_t *pc, void *data);
+static char *ngx_http_rl_balancer(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+static ngx_command_t ngx_http_rl_balancer_commands[] = {
+    { ngx_string("rl_balancer"),
+      NGX_HTTP_UPS_CONF|NGX_CONF_NOARGS,
+      ngx_http_rl_balancer,
+      0,
+      0,
+      NULL },
+
+      ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_rl_balancer_module_ctx = {
+    NULL,                                  /* preconfiguration */
+    NULL,                                  /* postconfiguration */
+
+    NULL,                                  /* create main configuration */
+    NULL,                                  /* init main configuration */
+
+    NULL,                                  /* create server configuration */
+    NULL,                                  /* merge server configuration */
+
+    NULL,                                  /* create location configuration */
+    NULL                                   /* merge location configuration */
+};
+
+ngx_module_t ngx_http_rl_balancer_module = {
+    NGX_MODULE_V1,
+    &ngx_http_rl_balancer_module_ctx,      /* module context */
+    ngx_http_rl_balancer_commands,         /* module directives */
+    NGX_HTTP_MODULE,                       /* module type */
+    NULL,                                  /* init master */
+    NULL,                                  /* init module */
+    NULL,                                  /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+typedef struct {
+    ngx_http_upstream_rr_peer_data_t rrp;
+} ngx_http_rl_balancer_peer_data_t;
+
+static ngx_int_t
+ngx_http_rl_balancer_init(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t *us)
+{
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->log, 0, "init rl balancer");
+
+    if (ngx_http_upstream_init_round_robin(cf, us) != NGX_OK) {
+        return NGX_ERROR;
+    }
+
+    us->peer.init = ngx_http_rl_balancer_init_peer;
+
+    return NGX_OK;
+}
+
+static ngx_int_t
+ngx_http_rl_balancer_init_peer(ngx_http_request_t *r, ngx_http_upstream_srv_conf_t *us)
+{
+    ngx_http_rl_balancer_peer_data_t *rlp;
+
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "init rl balancer peer");
+
+    rlp = ngx_palloc(r->pool, sizeof(ngx_http_rl_balancer_peer_data_t));
+    if (rlp == NULL) {
+        return NGX_ERROR;
+    }
+
+    r->upstream->peer.data = &rlp->rrp;
+
+    if (ngx_http_upstream_init_round_robin_peer(r, us) != NGX_OK) {
+        return NGX_ERROR;
+    }
+
+    r->upstream->peer.get = ngx_http_rl_balancer_get_peer;
+
+    return NGX_OK;
+}
+
+static ngx_int_t
+ngx_http_rl_balancer_get_peer(ngx_peer_connection_t *pc, void *data)
+{
+    ngx_http_rl_balancer_peer_data_t *rlp = data;
+    ngx_http_upstream_rr_peer_t *peer;
+    ngx_http_upstream_rr_peers_t *peers = rlp->rrp.peers;
+
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "get rl balancer peer, try: %ui", pc->tries);
+
+    if (peers->single) {
+        return ngx_http_upstream_get_round_robin_peer(pc, &rlp->rrp);
+    }
+
+    pc->cached = 0;
+    pc->connection = NULL;
+
+    // Инициализация ZeroMQ
+    void *context = zmq_ctx_new();
+    void *requester = zmq_socket(context, ZMQ_REQ);
+    zmq_connect(requester, "tcp://localhost:5555");
+
+    // Формируем запрос для агента RL
+    char request[256];
+    ngx_snprintf((u_char *)request, sizeof(request), "{\"state\": [%d, %d, %d, %d], \"next_state\": [%d, %d, %d, %d], \"reward\": %d, \"done\": %d}",
+                 1, 0, 0, 0,  // пример текущего состояния
+                 0, 1, 0, 0,  // пример следующего состояния
+                 1,           // пример награды
+                 0);          // пример завершения эпизода
+
+    zmq_send(requester, request, ngx_strlen(request), 0);
+
+    // Получаем ответ от агента
+    char buffer[10];
+    zmq_recv(requester, buffer, 10, 0);
+
+    int peer_index = ngx_atoi((u_char *)buffer, ngx_strlen(buffer));
+    zmq_close(requester);
+    zmq_ctx_destroy(context);
+
+    // Выбираем сервер по индексу, полученному от агента RL
+    ngx_uint_t i;
+    for (peer = peers->peer, i = 0; peer; peer = peer->next, i++) {
+        if (i == peer_index) {
+            pc->sockaddr = peer->sockaddr;
+            pc->socklen = peer->socklen;
+            pc->name = &peer->name;
+
+            peer->conns++;
+            rlp->rrp.current = peer;
+
+            ngx_http_upstream_rr_peer_lock(peers);
+            ngx_http_upstream_rr_peer_unlock(peers);
+
+            return NGX_OK;
+        }
+    }
+
+    ngx_http_upstream_rr_peers_unlock(peers);
+
+    return NGX_BUSY;
+}
+
+static char *
+ngx_http_rl_balancer(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_upstream_srv_conf_t *uscf;
+
+    uscf = ngx_http_conf_get_module_srv_conf(cf, ngx_http_upstream_module);
+
+    if (uscf->peer.init_upstream) {
+        ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "load balancing method redefined");
+    }
+
+    uscf->peer.init_upstream = ngx_http_rl_balancer_init;
+
+    uscf->flags = NGX_HTTP_UPSTREAM_CREATE
+                  |NGX_HTTP_UPSTREAM_WEIGHT
+                  |NGX_HTTP_UPSTREAM_MAX_CONNS
+                  |NGX_HTTP_UPSTREAM_MAX_FAILS
+                  |NGX_HTTP_UPSTREAM_FAIL_TIMEOUT
+                  |NGX_HTTP_UPSTREAM_DOWN
+                  |NGX_HTTP_UPSTREAM_BACKUP;
+
+    return NGX_CONF_OK;
+}
